05cb53d0e734bcd73553165a8cae5f5c28cec2e7
Smith <nsmith@aecio.com>
2018-04-04 08:47:30 -0400

    Added functionality to transactions component for submitting expense or income to Firebase

--- src/containers/Transactions/Transactions.js
+++ src/containers/Transactions/Transactions.js
@@ -1,3 +1,5 @@
+// TODO: Add authentication check, add table of transactions, link transactions to redux state, style component
+
 import React, { Component } from 'react'; 
 import { connect } from 'react-redux'; 
 
@@ -7,27 +9,64 @@ import DatePicker from 'material-ui/DatePicker';
 import TextField from 'material-ui/TextField';
 import SelectField from 'material-ui/SelectField';
 import MenuItem from 'material-ui/MenuItem';
-import { red100, green100 } from 'material-ui/styles/colors';
+import FloatingActionButton from 'material-ui/FloatingActionButton';
+import ContentAdd from 'material-ui/svg-icons/content/add';
+
+const CATEGORIES = [
+    'Entertainment', 
+    'Education', 
+    'Gas', 
+    'Food'
+]; 
+
+const TYPES = [
+    'Expense', 
+    'Income'
+]; 
 
 class Transactions extends Component {
 
     state = {
         newExpense: {
             date: null, 
-            amount: null, 
+            amount: "", 
             type: null, 
             category: null
         }
     }
 
     handleChange = (key, value) => {
-        console.log(value);
-        // this.setState({
-        //     newExpense: {
-        //         ...this.state.newExpense, 
-        //         [key]: value
-        //     }
-        // }); 
+        this.setState({
+            newExpense: {
+                ...this.state.newExpense, 
+                [key]: value
+            }
+        }); 
+    }
+
+    convertStateToDbValues() {
+        const postObj = {
+            date: {
+                month: this.state.newExpense.date.getMonth(), 
+                day: this.state.newExpense.date.getDate(), 
+                year: this.state.newExpense.date.getFullYear()
+            }, 
+            amount: +this.state.newExpense.amount, 
+            type: TYPES[this.state.newExpense.type], 
+            category: CATEGORIES[this.state.newExpense.category]
+        }
+
+        return postObj; 
+    }
+
+    sendNewTransaction = () => {
+        axios.post('https://react-finance-f20df.firebaseio.com/'+this.props.userProfile.sub+'.json', this.convertStateToDbValues())
+            .then(response => {
+                console.log(response);
+            }) 
+            .catch(err => {
+                console.log(err); 
+            })
     }
 
     render() {
@@ -35,28 +74,41 @@ class Transactions extends Component {
             <div>
                 <h1>Hello {this.props.userProfile.name} I am the Transactions Component</h1>
                 <DatePicker 
-                    intText="Date" 
+                    hintText="Date" 
+                    locale="en-US"
                     value={this.state.newExpense.date} 
-                    onChange={(event) => this.handleChange('date', event)} />
-                <TextField type="number" hintText="Amount" />
+                    onChange={(event, date) => this.handleChange('date', date)}
+                        
+                    />
+                <TextField 
+                    type="number" 
+                    hintText="Amount"
+                    step="0.01"
+                    value={this.state.newExpense.amount}
+                    onChange={(event) => this.handleChange('amount', event.target.value)} />
                 <div>
                     <SelectField
                         floatingLabelText="Type"
-                        value={this.state.newExpense.type} >
-                        <MenuItem style={{backgroundColor: red100}} value="expense" primaryText="Expense" />
-                        <MenuItem style={{backgroundColor: green100}} value="income" primaryText="Income" />
+                        value={this.state.newExpense.type}
+                        onChange={(event, key) => this.handleChange('type', key)} >
+                        <MenuItem value={0} primaryText="Expense" />
+                        <MenuItem value={1} primaryText="Income" />
                     </SelectField>
                 </div>
                 <div>
                     <SelectField
                         floatingLabelText="Category"
-                        value={this.state.newExpense.category} >
-                        <MenuItem value="entertainment" primaryText="Entertainment" />
-                        <MenuItem value="education" primaryText="Education" />
-                        <MenuItem value="gas" primaryText="Gas" />
-                        <MenuItem value="food" primaryText="Food" />
+                        value={this.state.newExpense.category}
+                        onChange={(event, key) => this.handleChange('category', key)} >
+                        <MenuItem value={0} primaryText="Entertainment" />
+                        <MenuItem value={1} primaryText="Education" />
+                        <MenuItem value={2} primaryText="Gas" />
+                        <MenuItem value={3} primaryText="Food" />
                     </SelectField>
                 </div>
+                <FloatingActionButton onClick={this.sendNewTransaction} >
+                    <ContentAdd />
+                </FloatingActionButton>
             </div>
         ); 
     }
