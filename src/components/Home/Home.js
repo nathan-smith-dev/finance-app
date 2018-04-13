3046a7c344a547c44b712e3fcb8af576d617be36
Smith <nsmith@aecio.com>
2018-04-13 09:08:46 -0400

    Fixed typo

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -21,7 +21,7 @@ class Home extends Component {
                             <h3 style={{marginBottom: 0}}>Welcome to Budget Space! </h3>
                             <p style={{fontSize: 14, marginTop: 8}}>Budget Space is a simple web application meant to make tracking your spending easy and fun. </p>
                             <ExampleCharts />
-                            <h4 style={{marginBottom: 0}}>Tracking Your Spending Easily</h4>
+                            <h4 style={{marginBottom: 0}}>Track Your Spending Easily</h4>
                             <p style={{fontSize: 14, marginTop: 8}}>Simply sign in with your Google account and enter your expenses and incomes for the selected month and year.</p>
                             <h4 style={{marginBottom: 0}}>Analyze Your Spending</h4>
                             <p style={{fontSize: 14, marginTop: 8}}>After entering transactions, take a look at the <em>Expenses / Income</em> or <em>Trends</em> page and see where your money has gone. </p>
