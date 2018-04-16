e3dff3ed540a51962c90dd2d66b4c3345ee4627b
Smith <nsmith@aecio.com>
2018-04-16 08:38:28 -0400

    Added puttting of profiles to DB on login

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,5 +1,7 @@
 import * as actionTypes from './actionTypes';
 import * as transactionActions from './transactions'; 
+import axios from 'axios'; 
+import { withAuth } from '../../firebase/auth'; 
 
 export const getProfileSuccess = (profile) => {
     return {
@@ -13,8 +15,31 @@ export const getProfile = (profile) => {
         if(profile) {
             dispatch(transactionActions.getTransactions(profile.uid, getState().transactions.trackedDates.month, getState().transactions.trackedDates.year));     
             dispatch(transactionActions.getTransactionCategories(profile.uid));     
+            dispatch(addProfileToDb(profile)); 
         }
-
+        
         dispatch(getProfileSuccess(profile)); 
     }
-}; 
+};
+
+export const addProfileToDb = (profile) => {
+    return dispatch => {
+        withAuth(authToken => {
+            const url = `profiles/${profile.uid}.json?auth=${authToken}`; 
+            axios.get(url)
+            .then(response => {
+                if(!response.data) {
+                    const profileObj = {
+                        email: profile.email, 
+                        name: profile.displayName
+                    }; 
+                    axios.put(url, profileObj); 
+                }
+            })
+            .catch(error => {
+                console.log(error); 
+            })
+        }); 
+        return; 
+    }
+}
