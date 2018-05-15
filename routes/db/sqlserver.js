f22a8955755e0dd7299e129f891df0262b03c56a
Smith <nsmith@aecio.com>
2018-05-15 15:09:45 -0400

    Boilerplate route to expenses, server jwt auth, and sql connection

---
+++ routes/db/sqlserver.js
@@ -0,0 +1,28 @@
+const sql = require('mssql'); 
+
+sql.on('error', err => {
+    console.log(err.message); 
+})
+
+const config = {
+    user: 'webapp',
+    password: 'Hps@051418',
+    server: 'localhost\\SQLEXPRESS', // You can use 'localhost\\instance' to connect to named instance
+    database: 'budget-space__test'
+}
+
+async function queryDataBase(query) {
+    try {
+        const pool = await sql.connect(config); 
+        const result = await pool.request()
+            .query(query); 
+        // console.log('[Successful Query]', result); 
+        sql.close(); 
+        return result; 
+    }
+    catch(err) {
+        console.log(err); 
+    }
+}
+
+module.exports = queryDataBase; 
