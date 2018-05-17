a65ec75d47312ff5216403c7f3eee9686da33abb
Smith <nsmith@aecio.com>
2018-05-17 09:18:29 -0400

    Added GET for category, income, and expense totals

--- server/routes/categories.js
+++ server/routes/categories.js
@@ -24,4 +24,28 @@ router.get('/', (req, res) => {
     });
 }); 
 
+router.get('/totals', (req, res) => {
+    const idToken = req.header('x-auth-token'); 
+    if(!idToken) return res.status(401).send('No auth token.'); 
+
+    verifyToken(idToken, decodedToken => {
+        let query = `EXEC GetUserCategoryTotals @userId = ${decodedToken.uid}`; 
+        if(req.query.year && req.query.month)
+            query += `, @date = '${+req.query.year}-${+req.query.month}-01'`
+
+        const result = queryDataBase(query); 
+        result.then(record => {
+            res.body = record.recordset; 
+            res.send(record.recordset); 
+        })
+        .catch(err => {
+            console.log(err.message)
+            res.status(500).send('Error completing request to server. '); 
+        })
+    }, err => {
+        res.status(401).send('Invalid token. ');         
+    });
+}); 
+
+
 module.exports = router; 
--- server/routes/incomes.js
+++ server/routes/incomes.js
@@ -26,6 +26,29 @@ router.get('/', (req, res) => {
     }, err => {
         res.status(401).send('Invalid token. ');         
     });
+});
+
+router.get('/totals', (req, res) => {
+    const idToken = req.header('x-auth-token'); 
+    if(!idToken) return res.status(401).send('No auth token.'); 
+
+    verifyToken(idToken, decodedToken => {
+        let query = `EXEC GetTotalIncomesAndExpenses @userId = ${decodedToken.uid}`; 
+        if(req.query.year && req.query.month)
+            query += `, @date = '${+req.query.year}-${+req.query.month}-01'`
+
+        const result = queryDataBase(query); 
+        result.then(record => {
+            res.body = record.recordset; 
+            res.send(record.recordset); 
+        })
+        .catch(err => {
+            console.log(err.message)
+            res.status(500).send('Error completing request to server. '); 
+        })
+    }, err => {
+        res.status(401).send('Invalid token. ');         
+    });
 }); 
 
 router.get('/:id', (req, res) => {
