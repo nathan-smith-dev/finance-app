ad48aa36f4413fdc461a6d961591f58b8d825e2e
Smith <nsmith@aecio.com>
2018-05-18 10:20:34 -0400

    Added allow-access-origin settings

--- server/server.js
+++ server/server.js
@@ -11,6 +11,14 @@ const app = express();
 
 app.use(express.json()); 
 app.use(helmet()); 
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, x-auth-token");
+    next();
+});
+  
 
 app.use('/api/expenses', expenses); 
 app.use('/api/incomes', incomes); 
