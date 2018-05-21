df2d6b1a5d83bef3734300f797877144901b1f89
Smith <nsmith@aecio.com>
2018-05-21 19:09:43 -0400

    Added GET for all categories

--- server/routes/categories.js
+++ server/routes/categories.js
@@ -47,5 +47,26 @@ router.get('/totals', (req, res) => {
     });
 }); 
 
+router.get('/all', (req, res) => {
+    const idToken = req.header('x-auth-token'); 
+    if(!idToken) return res.status(401).send('No auth token.'); 
+
+    verifyToken(idToken, decodedToken => {
+        let query = `EXEC GetAllCategories`; 
+
+        const result = queryDataBase(query); 
+        result.then(record => {
+            res.body = record.recordset; 
+            res.send(record.recordset); 
+        })
+        .catch(err => {
+            console.log(err.message);
+            res.status(500).send('Error completing request to server. '); 
+        })
+    }, err => {
+        res.status(401).send('Invalid token. ');         
+    });
+}); 
+
 
 module.exports = router; 
