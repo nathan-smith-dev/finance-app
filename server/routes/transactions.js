e63882bf6b5016f344cd98536515755d2d6ddb9c
Smith <nsmith@aecio.com>
2018-05-21 12:56:22 -0400

    Added api endpoint to get count of categories used for date

--- server/routes/transactions.js
+++ server/routes/transactions.js
@@ -28,13 +28,35 @@ router.get('/', (req, res) => {
     });
 }); 
 
-
 router.get('/dates', (req, res) => {
     const idToken = req.header('x-auth-token'); 
     if(!idToken) return res.status(401).send('No auth token.'); 
 
     verifyToken(idToken, decodedToken => {
         let query = `EXEC GetCountOfTransactionsByDate @userId = ${decodedToken.uid}`; 
+        if(req.query.year && req.query.month)
+            query += `, @date = '${+req.query.year}-${+req.query.month}-01'`;
+
+        const result = queryDataBase(query); 
+        result.then(record => {
+            res.body = record.recordset; 
+            res.send(record.recordset); 
+        })
+        .catch(err => {
+            console.log(err.message);
+            res.status(500).send('Error completing request to server. '); 
+        })
+    }, err => {
+        res.status(401).send('Invalid token. ');         
+    });
+}); 
+
+router.get('/categories', (req, res) => {
+    const idToken = req.header('x-auth-token'); 
+    if(!idToken) return res.status(401).send('No auth token.'); 
+
+    verifyToken(idToken, decodedToken => {
+        let query = `EXEC GetCountOfTransactionCategories @userId = ${decodedToken.uid}`; 
         if(req.query.year && req.query.month)
             query += `, @date = '${+req.query.year}-${+req.query.month}-01'`;
 
