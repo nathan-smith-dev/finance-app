998a98e2383508d2f45925ad4958303b79a60879
Nathan Smith <nis5294@gmail.com>
2018-05-27 14:39:05 -0400

    Added PUT for updating expenses and incomes

--- server/routes/expenses.js
+++ server/routes/expenses.js
@@ -2,6 +2,24 @@ const express = require('express');
 const router = express.Router(); 
 const queryDataBase = require('../db/sqlserver.js'); 
 const verifyToken = require('./auth'); 
+const Joi = require('joi'); 
+
+const schema = {
+    amount: Joi.number().required().positive(),
+    date: Joi.date().required(), 
+    desc: Joi.string().allow(''), 
+    categoryId: Joi.string().guid().required()
+}; 
+
+const formatDate = (date) => {
+    const adjustedDate = calcTimezoneOffset(date); 
+    return `${adjustedDate.getFullYear()}-${adjustedDate.getMonth() + 1}-${adjustedDate.getDate()}`; // add one to month because 0 index 
+}
+
+const calcTimezoneOffset = (date) => {
+    const userTimezoneOffset = date.getTimezoneOffset() * 60000;
+    return new Date(date.getTime() + userTimezoneOffset); 
+}
 
 router.get('/', (req, res) => {
     const idToken = req.header('x-auth-token'); 
@@ -60,6 +78,30 @@ router.get('/:id', (req, res) => {
     verifyToken(idToken, decodedToken => {
         console.log(req.params.id)
         let query = `EXEC GetUserExpense @expenseId = '${req.params.id}'`; 
+
+        const result = queryDataBase(query); 
+        result.then(record => {
+            res.body = record.recordset; 
+            res.send(record.recordset); 
+        })
+        .catch(err => {
+            console.log(err.message)
+            res.status(500).send('Error completing request to server. '); 
+        })
+    }, err => {
+        res.status(401).send('Invalid token. ');         
+    });
+}); 
+
+router.put('/:id', (req, res) => {
+    const idToken = req.header('x-auth-token'); 
+    if(!idToken) return res.status(401).send('No auth token.'); 
+
+    const { error, value } = Joi.validate(req.body, schema, { stripUnknown : true }); 
+    if(error) res.status(400).send(error.message);
+
+    verifyToken(idToken, decodedToken => {
+        let query = `EXEC UpdateExpense @expenseId = '${req.params.id}', @amount = ${value.amount}, @date = '${formatDate(value.date)}', @desc = '${value.desc}', @categoryId = '${value.categoryId}'`; 
 
         const result = queryDataBase(query); 
         result.then(record => {
