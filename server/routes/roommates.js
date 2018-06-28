36882f2db3395f285283b54b43039c07a07be2eb
Nathan Smith <nis5294@gmail.com>
2018-06-27 22:01:58 -0400

    Added delete to roommate routes on API

--- server/routes/roommates.js
+++ server/routes/roommates.js
@@ -251,4 +251,26 @@ router.get('/expenses/user/:id', (req, res) => {
     });
 }); 
 
+router.delete('/:id', (req, res) => {
+    const idToken = req.header('x-auth-token'); 
+    if(!idToken) return res.status(401).send('No auth token.'); 
+
+    verifyToken(idToken, decodedToken => {
+        console.log(req.params.id)
+        let query = `EXEC DeleteRoommate @id1 = '${req.params.id}', @id2 = ${decodedToken.uid}`; 
+
+        const result = queryDataBase(query); 
+        result.then(record => {
+            res.body = record.recordset; 
+            res.send(record.recordset); 
+        })
+        .catch(err => {
+            console.log(err.message)
+            res.status(500).send('Error completing request to server. '); 
+        })
+    }, err => {
+        res.status(401).send('Invalid token. ');         
+    });
+}); 
+
 module.exports = router; 
